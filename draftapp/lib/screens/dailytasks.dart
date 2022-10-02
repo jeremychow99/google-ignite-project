f06f1bac4db4e41c872ad9be1862ca441da8dc2d
jeremy chow <xelloxzxd@gmail.com>
2022-10-02 20:04:41 +0800

    add debug:false

--- draftapp/lib/screens/dailytasks.dart
+++ draftapp/lib/screens/dailytasks.dart
@@ -9,6 +9,7 @@ class ToDoList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
         title: 'test',
         home: Scaffold(
           drawer: const NavigationDrawer(),
